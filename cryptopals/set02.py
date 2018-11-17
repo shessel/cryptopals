2d7f1d7f8071249e75a2b9a7c27801c4c5e0831e
shessel <shessel@users.noreply.github.com>
2018-11-17 13:43:47 +0100

    add set 02 challenge 10: implement AES CBC

--- cryptopals/set02.py
+++ cryptopals/set02.py
@@ -1,5 +1,36 @@
+from Crypto.Cipher import AES
+from cryptopals import set01
+
 def pkcs7_padding(input, block_size):
     num_blocks = len(input) // block_size
     last_block_size = len(input) - num_blocks * block_size
     num_pad_bytes = block_size - last_block_size
     return input + bytes([num_pad_bytes] * num_pad_bytes)
+
+def encrypt_aes_ecb(key, text):
+    return AES.new(key, AES.MODE_ECB).encrypt(text)
+
+def encrypt_aes_cbc(key, text, iv = bytes([0] * 16)):
+    assert len(key) == 16
+    assert len(iv) == 16
+    xor_block = iv
+    encrypted_text = b''
+    for block_start in range(0, len(text), 16):
+        xored_block = bytes([text[block_start + i] ^ xor_block[i] for i in range(16)])
+        encrypted_block = encrypt_aes_ecb(key, xored_block)
+        encrypted_text += encrypted_block
+        xor_block = encrypted_block
+    return encrypted_text
+
+def decrypt_aes_cbc(key, encrypted_text, iv = bytes([0] * 16)):
+    assert len(key) == 16
+    assert len(iv) == 16
+    xor_block = iv
+    decrypted_text = b''
+    for block_start in range(0, len(encrypted_text), 16):
+        encrypted_block = encrypted_text[block_start:block_start+16]
+        decrypted_block = set01.decrypt_aes_ecb(key, bytes(encrypted_block))
+        xored_block = bytes([decrypted_block[i] ^ xor_block[i] for i in range(16)])
+        decrypted_text += xored_block
+        xor_block = encrypted_block
+    return decrypted_text
--- tests/test_set02.py
+++ tests/test_set02.py
@@ -1,4 +1,5 @@
 import unittest
+from cryptopals import set01
 from cryptopals import set02
 
 class Pkcs7PaddingTests(unittest.TestCase):
@@ -10,3 +11,51 @@ class Pkcs7PaddingTests(unittest.TestCase):
 
     def test_cryptopals(self):
         self.assertEqual(set02.pkcs7_padding(b'YELLOW SUBMARINE', 20), b'YELLOW SUBMARINE\x04\x04\x04\x04')
+
+class AesCbcEncryptTests(unittest.TestCase):
+    def test_single_block_zero_iv(self):
+        input = b'abcdefghijklmnop'
+        key = b'foobarfoobarfoob'
+        expected_output = set02.encrypt_aes_ecb(key, input)
+        self.assertEqual(set02.encrypt_aes_cbc(key, input), expected_output)
+
+    def test_cryptopals(self):
+        with open('output/decrypt-aes-cbc.txt', 'rb') as output_file:
+            output_bytes = output_file.read()
+        input_base64 = set01.read_base64_from_file('input/decrypt-aes-cbc.txt')
+        input_bytes = set01.base64_to_bytes(input_base64)
+        self.assertEqual(set02.encrypt_aes_cbc(b'YELLOW SUBMARINE', output_bytes), input_bytes)
+
+class AesCbcDecryptTests(unittest.TestCase):
+    def test_single_block_zero_iv(self):
+        input = b'abcdefghijklmnop'
+        key = b'foobarfoobarfoob'
+        encrypted_input = set02.encrypt_aes_cbc(key, input)
+        self.assertEqual(set02.decrypt_aes_cbc(key, encrypted_input), input)
+
+    def test_single_block(self):
+        input = b'abcdefghijklmnop'
+        key = b'foobarfoobarfoob'
+        iv = b'narfnarfnarfnarf'
+        encrypted_input = set02.encrypt_aes_cbc(key, input, iv)
+        self.assertEqual(set02.decrypt_aes_cbc(key, encrypted_input, iv), input)
+
+    def test_multi_block_zero_iv(self):
+        input = b'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/+'
+        key = b'foobarfoobarfoob'
+        encrypted_input = set02.encrypt_aes_cbc(key, input)
+        self.assertEqual(set02.decrypt_aes_cbc(key, encrypted_input), input)
+
+    def test_multi_block(self):
+        input = b'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/+'
+        key = b'foobarfoobarfoob'
+        iv = b'narfnarfnarfnarf'
+        encrypted_input = set02.encrypt_aes_cbc(key, input, iv)
+        self.assertEqual(set02.decrypt_aes_cbc(key, encrypted_input, iv), input)
+
+    def test_cryptopals(self):
+        input_base64 = set01.read_base64_from_file('input/decrypt-aes-cbc.txt')
+        input_bytes = set01.base64_to_bytes(input_base64)
+        with open('output/decrypt-aes-cbc.txt', 'rb') as output_file:
+            output_bytes = output_file.read()
+        self.assertEqual(set02.decrypt_aes_cbc(b'YELLOW SUBMARINE', input_bytes), output_bytes)
